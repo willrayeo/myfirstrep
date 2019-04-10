e87fc74fca9150a60c519caa36ae444671210ddb
William Ray <williamray9@gmail.com>
2019-04-10 18:55:08 +0100

    updated sen2_rgbcorrection.py

--- sen2_rgbcorrection.py
+++ sen2_rgbcorrection.py
@@ -2,20 +2,51 @@
 import os
 from zipfile import ZipFile
 
-# define variables
-folder = "/Users/williamray/Downloads/Biocarbon_python_test/S2A_MSIL2A_20180901T105621_N0208_R094_T31UDQ_20180901T162709.SAFE/GRANULE/L2A_T31UDQ_A016679_20180901T105939/IMG_DATA/R10m/"
-R = folder+"T31UDQ_20180901T105621_B04_10m.jp2"
-G = folder+"T31UDQ_20180901T105621_B03_10m.jp2"
-B = folder+"T31UDQ_20180901T105621_B02_10m.jp2"
-rgbimage = "rgb.tif"
-rgb_color_corrected = "rgb_corrected.tif"
+class Sen2colorcorrection():
+    """
+    Class that collects methods of CNNClassifier class
 
-# unzip sentinel 2 zip file
-sen2zip = ZipFile('S2A_MSIL2A_20180901T105621_N0208_R094_T31UDQ_20180901T162709.zip', 'r')
-sen2zip.extractall('')
+    class attributes:
+    zipfile = the sentinel 2 zipfile downloaded from openhub. Can sit in same directory\
+    as the script, otherwise specify full file pathself.
 
-# stack individual bands into an RGB image
-os.system("gdal_merge.py -n 0 -a_nodata 0 -separate -of GTiff -o %s %s %s %s" % (rgbimage, R, G, B))
+    """
 
-# color correct the RGB image using rio color https://github.com/mapbox/rio-color
-os.system("rio color -d uint16 -j 4 %s %s gamma G 1.1 gamma B 1.2 sigmoidal RGB 6 0.5 saturation 1.15" % (rgbimage, rgb_color_corrected))
+    # Initializer / Instance Attributes
+    def __init__(self,
+                 zipfile,
+                 rgb_color_corrected,
+                 R = "*.SAFE/GRANULE/*/IMG_DATA/*_B04.jp2",
+                 G = "*.SAFE/GRANULE/*/IMG_DATA/*_B03.jp2",
+                 B = "*.SAFE/GRANULE/*/IMG_DATA/*_B02.jp2",
+                 rgbimage = "rgb.tif"):
+        self.unzip(zipfile)
+        ok = self.stack(rgbimage, R, G, B)
+        self.correct(rgbimage, rgb_color_corrected)
+
+    # unzip sentinel 2 zip file
+
+    def unzip(self, zipfile):
+        sen2zip = ZipFile(zipfile, 'r')
+        sen2zip.extractall('')
+
+    # stack individual bands into an RGB image
+
+    def stack(self, rgbimage, R, G, B):
+        success = os.system("gdal_merge.py -n 0 -a_nodata 0 -separate -of GTiff -o %s %s %s %s" % (rgbimage, R, G, B))
+        return success
+    # color correct the RGB image using rio color https://github.com/mapbox/rio-color
+    def correct(self, rgbimage, rgb_color_corrected):
+        success = os.system("rio color -d uint16 -j 4 %s %s gamma G 1.1 gamma B 1.2 sigmoidal RGB 6 0.5 saturation 1.15" % (rgbimage, rgb_color_corrected))
+        print("your image has been corrected")
+        return success
+
+# Variables as strings
+#zipfile = '/home/kore-dev/Documents/objects/L1C_T18TWL_A016310_20180806T154402.zip'
+#folder = "*.SAFE/GRANULE/*/IMG_DATA/"
+#R = folder+"*_B04.jp2"
+#G = folder+"*_B03.jp2"
+#B = folder+"*_B02.jp2"
+
+
+Sen2colorcorrection('L1C_T18TWL_A016310_20180806T154402.zip', 'mycorrectedimg.tif')
